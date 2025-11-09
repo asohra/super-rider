ccd095132664c63f5c0882b20a2fa8eea1596072
Armin Raad <armin3600z@gmail.com>
2025-11-09 13:30:30 +0000

    add ui module
    Squash merge penpot_ui_loading into main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
-fn main() {
-    println!("Hello, world!");
-}
+mod ui;
+
+// Program entry point.
+fn main() {}
---
+++ src/ui/mod.rs
@@ -0,0 +1,13 @@
+#[derive(Debug)]
+pub struct Object {
+    id: String,
+}
+
+// Load from penpot file.
+pub fn load_penpot_file() {
+    // Check if extracted directory exists.
+    // Check if penpot file exists.
+    // If penpot file exists and penportfile.lastmodified != extracteddirectory.lastmodified
+    //      extract pentpotfile into extracteddirectory
+    // Load objects from extracteddirectory.
+}
